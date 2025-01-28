0df03fc325ffc818293eb687d1ceef67416b2d2c
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2025-01-28 20:43:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,29 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import marked from 'marked'; 
 
+function App() {
+  const [markdown, setMarkdown] = useState('');
+  const [preview, setPreview] = useState('');
+
+  useEffect(() => {
+    const newPreview = marked(markdown);
+    setPreview(newPreview);
+  }, [markdown]); 
+
+  return (
+    <div className="app">
+      <div className="textarea">
+        <textarea 
+          value={markdown} 
+          onChange={(e) => setMarkdown(e.target.value)} 
+        />
+      </div>
+      <div className="preview">
+        <div dangerouslySetInnerHTML={{ __html: preview }} /> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
